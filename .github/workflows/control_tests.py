ac4576e3b7a70e122f5715da43173cc9231be3b4
Ubuntu <ubuntu@ip-172-31-7-42.eu-west-1.compute.internal>
2024-11-28 10:47:12 +0000

    new pytest and flake8 setup

--- .github/workflows/control_tests.py
+++ .github/workflows/control_tests.py
@@ -1,8 +1,5 @@
 name: CI Workflow Pytest and Flake8
-on:
-  push:
-    branches:
-      - main
+on: push
 jobs:
   qa:
     name: Run Tests and Linting
@@ -10,15 +7,19 @@ jobs:
     steps:
       - name: Checkout code
         uses: actions/checkout@v3
+      
       - name: Set up Python
         uses: actions/setup-python@v4
         with:
           python-version: "3.x"
+
       - name: Install dependencies
         run: |
           python -m pip install --upgrade pip
           pip install pytest flake8
+
       - name: Run tests
         run: pytest tests/
+
       - name: Run flake8
         run: flake8 .
